72cc9108345c70410d3e614bebd86ab726dada63
Kid-Guru <birkov.alexandr@gmail.com>
2021-03-30 16:40:30 +0400

    change appBar designe

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -6,8 +6,8 @@ const AppBar = () => {
   return (
     <Navbar bg="light" variant="light">
       <Container>
-        <Navbar.Brand as={'h1'}>Navbar</Navbar.Brand>
-        <Nav variant="tabs" className="mr-auto">
+        <Navbar.Brand as={'h1'} className="mr-auto">SoccerStat App</Navbar.Brand>
+        <Nav variant="pills">
           <Nav.Link as={NavLink} to="/competitions">Турниры</Nav.Link>
           <Nav.Link as={NavLink} to="/teams">Команды</Nav.Link>
         </Nav>
